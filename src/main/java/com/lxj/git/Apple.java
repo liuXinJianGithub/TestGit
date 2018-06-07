1e977d399436d1d80271a57a3c9395bf88f24a18
eclipse_user <eclipse_user@liuxinjian.com>
2018-06-08 00:39:49 +0800

    My first Commit in eclipse

---
+++ src/main/java/com/lxj/git/Apple.java
@@ -0,0 +1,10 @@
+package com.lxj.git;
+
+public class Apple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("APPLE APLLE");
+	}
+
+}
---
+++ src/test/java/testGit/GoodTest.java
@@ -0,0 +1,9 @@
+package testGit;
+
+public class GoodTest {
+
+	public static void main(String[] args) {
+          System.out.println("Good Test!!!!");
+	}
+
+}
