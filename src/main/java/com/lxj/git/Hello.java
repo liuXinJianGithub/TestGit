6e4b0e6d8d833019073faa0b11b7bfa69034e896
eclipse_user <eclipse_user@liuxinjian.com>
2018-06-09 00:46:17 +0800

    ttttt

--- src/main/java/com/lxj/git/Hello.java
+++ src/main/java/com/lxj/git/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
         
         System.out.println("lest.......................");
-        System.out.println("hot fix.....................");
+        System.out.println("hot fix.....................fdsfdsf");
 	}
 
 }
